a46feeb6170417aa5e3473760a2f88cb86ec0d5e
sylsylviane <syl.sylviane1@gmail.com>
2025-01-16 08:49:15 -0500

    Créer la classe View

---
+++ providers/View.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+class View
+{
+    static public function render($template, $data = [])
+    {
+        $loader = new FilesystemLoader('views');
+        $twig = new Environment($loader);
+        $twig->addGlobal('asset', ASSET);
+        $twig->addGlobal('base', BASE);
+        // if (isset($_SESSION['finger_print']) and $_SESSION['finger_print'] === md5($_SERVER['HTTP_USER_AGENT'] . $_SERVER['REMOTE_ADDR'])) {
+            // $guest = false;
+        // } else {
+            // $guest = true;
+        // }
+        // $twig->addGlobal('guest', $guest);
+        // $twig->addGlobal('session', $_SESSION);
+        echo $twig->render($template . ".php", $data);
+    }
+
+    static public function redirect($url)
+    {
+        header('location:' . BASE . '/' . $url);
+    }
+}
