502294d00d9fc54bec424a5c731fb217dcd3efdf
sylsylviane <syl.sylviane1@gmail.com>
2025-01-22 14:16:12 -0500

    Créer la page d'erreur

---
+++ views/error.php
@@ -0,0 +1,7 @@
+{{include('layouts/header.php', {title:'Error 404'})}}
+<div class="container">
+    <h2>Error</h2>
+    <strong class="error">404 page not found ! </strong>
+    <p>{{msg}}</p>
+</div>
+{{include('layouts/footer.php')}}
