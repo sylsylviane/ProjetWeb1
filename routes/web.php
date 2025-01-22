f14d49aa4667909230f5d0af31a50cc061f88cd1
sylsylviane <syl.sylviane1@gmail.com>
2025-01-22 13:24:22 -0500

    Modifier les routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,22 @@
 use App\Routes\Route;
 use App\Controllers\AccueilController;
 use App\Controllers\PortailController;
+use App\Controllers\AuthController;
+use App\Controllers\MembreController;
+
 
 Route::get('/accueil', 'AccueilController@index');
 Route::get('/portail-encheres', 'PortailController@index');
+
+Route::post('/membre/inscription', 'MembreController@store');
+Route::get('/membre/inscription', 'MembreController@create');
+Route::get('/membre/profil', 'MembreController@show');
+Route::get('/membre/edit', 'MembreController@edit');
+Route::post('/membre/edit', 'MembreController@update');
+
+Route::get('/login', 'AuthController@index');
+Route::post('/login', 'AuthController@store');
+Route::get('/logout', 'AuthController@delete');
+
 Route::dispatch();
 ?>
