c757eaa10bfebf9d28c822068cae2937032dce51
sylsylviane <syl.sylviane1@gmail.com>
2025-01-22 10:25:13 -0500

    Créer la classe Membre

---
+++ models/Membre.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Models;
+use App\Models\CRUD;
+
+class Membre extends CRUD{
+    protected $table = 'membre';
+    protected $primaryKey = 'id';
+    protected $fillable = ['prenom', 'nom_famille', 'nom_utilisateur', 'mdp', 'courriel', 'telephone', 'adresse', 'code_postal', 'ville', 'province','pays_id'];
+
+    public function hashPassword ($password, $cost = 10){
+        $options = ['cost'=>$cost];
+        return password_hash($password, PASSWORD_BCRYPT, $options);
+    }
+
+    public function checkuser($username, $password)
+    {
+        $membre = $this->unique('nom_utilisateur', $username);
+        if ($membre) {
+            if (password_verify($password, $membre['mot_de_passe'])) {
+                session_regenerate_id();
+                $_SESSION['user_id'] = $membre['id'];
+                $_SESSION['user_name'] = $membre['prenom'];
+                $_SESSION['finger_print'] = md5($_SERVER['HTTP_USER_AGENT'] . $_SERVER['REMOTE_ADDR']);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
