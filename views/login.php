df95d6cc9d5736ec95da4f7065b3068051307759
sylsylviane <syl.sylviane1@gmail.com>
2025-01-22 14:15:09 -0500

    Créer formulaire de login

---
+++ views/login.php
@@ -0,0 +1,31 @@
+{{include('layouts/header.php', {title: 'Connexion'})}}
+<div>
+    <h2>Connexion</h2>
+    <form method="post" class="formulaire">
+
+        {% if errors is defined %}
+
+        {% for error in errors %}
+        <span>{{ error }}</span>
+        {% endfor %}
+
+        {% endif %}
+        <label class="champ-input">Nom d'utilisateur
+            <input type="email" name="nom_utilisateur" value="{{ inputs.nom_utilisateur }}">
+        </label>
+
+        <label class="champ-input">Mot de passe
+            <input type="password" name="mdp">
+        </label>
+
+        <label class="champ-input">Confirmation du mot de passe
+            <input type="password" name="mdp2">
+        </label>
+
+        <a href="">Mot de passe oublié ?</a>
+        <input type="submit" class="bouton" value="Accéder à mon profil">
+    </form>
+</div>
+{{include('layouts/footer.php')}}
+
+<!-- https://www.youtube.com/watch?v=NlAy5K-txQs&list=PLi_8oNFfVFrzROlhqbUdI0uCugO55RMkz&index=31 -->
