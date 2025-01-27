9469ae0ada921db427e0aad5346dadb5df68d69e
sylsylviane <syl.sylviane1@gmail.com>
2025-01-27 09:52:45 -0500

    Ajouter un répertoire pour les vues du mot de passe oublié

---
+++ views/resetpwd/email-send-successfully.php
@@ -0,0 +1,6 @@
+{{include('layouts/header.php', {title:'Error 404'})}}
+<div class="container">
+    <h2>Bravo !</h2>
+    <p>Un email a été envoyé avec un nouveau token </p>
+</div>
+{{include('layouts/footer.php')}}
