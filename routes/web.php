c2ba463ba34e01bd59bff3c57246805fddf28389
sylsylviane <syl.sylviane1@gmail.com>
2025-01-27 09:36:47 -0500

    Ajouter les routes pour le mot de passe oublié

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
-use App\Routes\Route;
+use App\Routes\Route; 
 use App\Controllers\AccueilController;
 use App\Controllers\PortailController;
 use App\Controllers\AuthController;
 use App\Controllers\MembreController;
-
+use App\Controllers\ResetPassword;
+ 
 
 Route::get('/accueil', 'AccueilController@index');
 Route::get('/portail-encheres', 'PortailController@index');
 
-Route::post('/membre/inscription', 'MembreController@store');
 Route::get('/membre/inscription', 'MembreController@create');
+Route::post('/membre/inscription', 'MembreController@store');
+
 Route::get('/membre/profil', 'MembreController@show');
 Route::get('/membre/edit', 'MembreController@edit');
 Route::post('/membre/edit', 'MembreController@update');
@@ -18,6 +20,12 @@ Route::post('/membre/edit', 'MembreController@update');
 Route::get('/login', 'AuthController@index');
 Route::post('/login', 'AuthController@store');
 Route::get('/logout', 'AuthController@delete');
+
+Route::get('/resetpwd/forgot-password', 'ResetPassword@index');
+Route::post('/resetpwd/forgot-password', 'ResetPassword@token');
+
+Route::get('resetpwd/reset-password', 'ResetPassword@recuperationToken');
+Route::post('resetpwd/reset-password', 'ResetPassword@resetPassword'); 
 
 Route::dispatch();
 ?>
