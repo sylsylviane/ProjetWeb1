63113ae8e9424f3df7f6ed176621662523300a24
sylsylviane <syl.sylviane1@gmail.com>
2025-01-29 17:12:58 -0500

    Ajouter des routes pour la saisie des timbres

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use App\Controllers\PortailController;
 use App\Controllers\AuthController;
 use App\Controllers\MembreController;
 use App\Controllers\ResetPassword;
+use App\Controllers\TimbreController;
+use App\Controllers\ImageController;
  
 
 Route::get('/accueil', 'AccueilController@index');
-Route::get('/portail-encheres', 'PortailController@index');
+// Route::get('/portail-encheres', 'PortailController@index');
 
 Route::get('/membre/inscription', 'MembreController@create');
 Route::post('/membre/inscription', 'MembreController@store');
@@ -26,6 +28,18 @@ Route::post('/resetpwd/forgot-password', 'ResetPassword@token');
 
 Route::get('/resetpwd/reset-password', 'ResetPassword@recuperationToken');
 Route::post('/resetpwd/reset-password', 'ResetPassword@resetPassword'); 
+
+Route::get('/timbre', 'TimbreController@index');
+Route::get('/timbre/create', 'TimbreController@create');
+Route::post('/timbre/create', 'TimbreController@store');
+Route::get('/timbre/show', 'TimbreController@show');
+Route::get('/timbre/edit', 'TimbreController@edit');
+Route::post('/timbre/edit', 'TimbreController@update');
+Route::post('/timbre/delete', 'TimbreController@delete');
+
+
+Route::get('/timbre/upload-img', 'ImageController@index');
+Route::post('/timbre/upload-img', 'ImageController@uploadImage');
 
 Route::dispatch();
 ?>
