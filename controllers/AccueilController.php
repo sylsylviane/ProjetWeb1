edb3e0c30898e72d8fee1228a7f1450de5dbc2a3
sylsylviane <syl.sylviane1@gmail.com>
2025-01-29 17:08:06 -0500

    Modifier la fonction index pour afficher les nouveaux timbres sur l'accueil

--- controllers/AccueilController.php
+++ controllers/AccueilController.php
@@ -1,11 +1,38 @@
 <?php
+
 namespace App\Controllers;
 
+use App\Models\Timbre;
 use App\Providers\View;
+use App\Models\Condition;
+use App\Models\Couleur;
+use App\Models\Pays;
+use App\Models\Image;
 
-class AccueilController {
-    public function index(){
-        return View::render('accueil');
+class AccueilController
+{
 
+    public function index()
+    {
+        $timbre = new Timbre;
+        $timbres = $timbre->getSixNewest();
+
+        $paysModel = new Pays;
+        $pays = $paysModel->select('nom');
+
+        $condition = new Condition;
+        $conditions = $condition->select('nom');
+
+        $couleur = new Couleur;
+        $couleurs = $couleur->select('nom');
+
+        $image = new Image;
+        $images = $image->select('timbre_id');
+
+        if ($timbres) {
+            return View::render('accueil', ['timbres' => $timbres, 'pays' => $pays, 'conditions' => $conditions, 'couleurs' => $couleurs, 'images' => $images]);
+        } else {
+            return View::render('error');
+        }
     }
 }
