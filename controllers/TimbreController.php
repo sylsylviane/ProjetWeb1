5f56f061ff0199d1ffb38912abaada8567a601b8
sylsylviane <syl.sylviane1@gmail.com>
2025-01-29 17:10:01 -0500

    Créer les méthodes de TimbreController pour la saise des timbres

---
+++ controllers/TimbreController.php
@@ -0,0 +1,191 @@
+<?php
+namespace App\Controllers;
+use App\Providers\View;
+use App\Providers\Auth;
+use App\Models\Pays;
+use App\Models\Condition;
+use App\Models\Couleur;
+use App\Providers\Validator;
+use App\Models\Timbre;
+use App\Models\Image;
+
+class TimbreController
+{
+    // //Vérifie l'authentification dans le constructeur de UserController, garantissant qu'aucune des méthodes de TimbreController n'est accessible sans authentification.
+    public function __construct()
+    {
+        Auth::session();
+    }   
+    
+    public function index()
+    {
+        return View::render('timbre/index');
+    }
+
+    public function create(){
+        $paysModel = new Pays;
+        $pays = $paysModel->select('nom');
+
+        $condition = new Condition;
+        $conditions = $condition->select('nom');
+
+        $couleur = new Couleur;
+        $couleurs = $couleur->select('nom');
+        if ($pays && $conditions) {
+            return View::render('timbre/create', ['pays' => $pays, 'conditions' => $conditions, 'couleurs' => $couleurs]);
+        } else {
+            return View::render('error');
+        }
+    }
+
+    public function store($data){
+        $validator = new Validator;
+        $validator->field('nom', $data['nom'])->required();
+        $validator->field('date', $data['date'])->required();
+        $validator->field('tirage', $data['tirage'])->required();
+        $validator->field('dimension', $data['dimension'])->required();
+        $validator->field('certification', $data['certification'])->required();
+        $validator->field('description', $data['description'])->required();
+        $validator->field('couleur_id', $data['couleur_id'], 'Couleur')->required();
+        $validator->field('pays_id', $data['pays_id'], 'Pays')->required();
+        $validator->field('condition_id', $data['condition_id'], 'Condition')->required();
+
+        if ($validator->isSuccess()){
+            $timbre = new Timbre;
+            $data['membre_id'] = $_SESSION['user_id'];
+
+            $insert = $timbre->insert($data);
+
+            if ($insert){
+                return View::redirect('timbre/upload-img?id='.$insert);
+            } else {
+                return View::render('error');
+                
+            }
+            
+        }else{
+            $errors = $validator->getErrors();
+            $paysModel = new Pays;
+            $pays = $paysModel->select('nom');
+
+            $condition = new Condition;
+            $conditions = $condition->select('nom');
+
+            $couleur = new Couleur;
+            $couleurs = $couleur->select('nom');
+
+            return View::render('timbre/create', ['errors' => $errors, 'inputs' => $data, 'pays' => $pays, 'conditions' => $conditions, 'couleurs' => $couleurs]);
+        }
+    }
+
+    public function show(){
+
+            $timbre = new Timbre;
+            $timbres = $timbre->selectByField('membre_id', $_SESSION['user_id']);
+
+            $paysModel = new Pays;
+            $pays = $paysModel->select('nom');
+
+            $condition = new Condition;
+            $conditions = $condition->select('nom');
+
+            $couleur = new Couleur;
+            $couleurs = $couleur->select('nom');
+
+            $image = new Image;
+            $images = $image->select('timbre_id');
+
+            if ($timbres){
+                return View::render('timbre/show', ['timbres' => $timbres, 'conditions' => $conditions, 'couleurs' => $couleurs, 'images' => $images, 'pays' => $pays]);
+            } else {
+                return View::render('timbre/show', ['msg' => 'Vous n\'avez aucun timbre.']);
+            }
+    }
+
+    public function edit($data = [])
+    {
+
+        if (isset($data['id']) && $data['id'] != null) {
+            $timbre = new Timbre;
+            $selectId = $timbre->selectId($data['id']);
+            if ($selectId) {
+                $paysModel = new Pays;
+                $pays = $paysModel->select('nom');
+
+                $condition = new Condition;
+                $conditions = $condition->select('nom');
+
+                $couleur = new Couleur;
+                $couleurs = $couleur->select('nom');
+
+                $image = new Image;
+                $images = $image->select('timbre_id');
+
+                return View::render('timbre/edit', ['pays' => $pays, 'inputs' => $selectId, 'conditions' => $conditions, 'couleurs' => $couleurs, 'images' => $images ]);
+            } else {
+                return View::render('error');
+            }
+        }
+        return View::render('login');
+    }
+
+    public function update($data = [], $get = [])
+    {
+        if (isset($get['id']) && $get['id'] != null) {
+            $validator = new Validator;
+            $validator->field('nom', $data['nom'])->required();
+            $validator->field('date', $data['date'])->min(4)->max(4)->required();
+            $validator->field('tirage', $data['tirage'])->required();
+            $validator->field('dimension', $data['dimension'])->required();
+            $validator->field('certification', $data['certification'])->required();
+            $validator->field('description', $data['description'])->required();
+            $validator->field('couleur_id', $data['couleur_id'], 'Couleur')->required();
+            $validator->field('pays_id', $data['pays_id'], 'Pays')->required();
+            $validator->field('condition_id', $data['condition_id'], 'Condition')->required();
+
+            if ($validator->isSuccess()) {
+                $timbre = new Timbre;
+                $update = $timbre->update($data, $get['id']);
+                if ($update) {
+                    return View::redirect('timbre/upload-img?id='.$get['id']);
+                } else {
+                    return View::render('error');
+                }
+            } else {
+                $errors = $validator->getErrors();
+                $paysModel = new Pays;
+                $pays = $paysModel->select('nom');
+
+                $condition = new Condition;
+                $conditions = $condition->select('nom');
+
+                $couleur = new Couleur;
+                $couleurs = $couleur->select('nom');
+
+                $image = new Image;
+                $images = $image->select('timbre_id');
+
+                return View::render('timbre/edit', ['errors' => $errors, 'inputs' => $data, 'pays' => $pays, 'conditions' => $conditions, 'couleurs' => $couleurs]);
+            }
+        }
+        return View::render('error');
+    }
+
+    //Méthode pour supprimer les timbres et les images associées
+
+    // public function delete($data)
+    // {
+    //     $timbre = new Timbre;
+    //     $image = new Image;
+
+    //     $delete = $timbre->delete($data['id']);
+
+    //     //Sélectionner l'image dont timbre_id = $data['id']
+
+    //     if ($delete) {
+            
+    //         return View::redirect('timbre/show');
+    //     }
+    //     return View::render('error');
+    // }
+}
