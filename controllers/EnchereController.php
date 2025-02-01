4f0eaba0b6acb49c9fd7e957e502f7d239ce0800
sylsylviane <syl.sylviane1@gmail.com>
2025-02-01 14:41:05 -0500

    Modifier pour ajouter l'offre actuelle des mises dans les vues

--- controllers/EnchereController.php
+++ controllers/EnchereController.php
@@ -9,6 +9,7 @@ use App\Models\Timbre;
 use App\Models\Condition;
 use App\Models\Couleur;
 use App\Models\Pays;
+use App\Models\Mise;
 
 class EnchereController
 {
@@ -30,38 +31,47 @@ class EnchereController
 
         $condition = new Condition;
         $conditions = $condition->select('nom');
+
+        $mise = new Mise;
+        $mises = $mise->select('montant');
         
         if ($encheres) {
-            return View::render('portail-encheres', ['encheres' => $encheres, 'images' => $images, 'timbres' => $timbres, 'conditions' => $conditions]);
+            return View::render('portail-encheres', ['encheres' => $encheres, 'images' => $images, 'timbres' => $timbres, 'conditions' => $conditions, 'mises' => $mises]);
         } else {
             return View::render('error');
         }
     }
 
-    public function showEnchere(){
+    public function showEnchere($data = []){
 
-            $enchere = new Enchere;
-            $encheres = $enchere->selectByField('timbre_id', $_GET['id']);
+            if (isset($data['id']) && $data['id'] != null){
+                $enchere = new Enchere;
+                $encheres = $enchere->selectByField('id', $_GET['id']);
 
-            $timbre = new Timbre;
-            $timbres = $timbre->select('nom');
+                $timbre = new Timbre;
+                $timbres = $timbre->select('nom');
 
-            $image = new Image;
-            $images = $image->select('timbre_id');
+                $image = new Image;
+                $images = $image->select('timbre_id');
 
-            $condition = new Condition;
-            $conditions = $condition->select('nom');
+                $condition = new Condition;
+                $conditions = $condition->select('nom');
 
-            $paysModel = new Pays;
-            $pays = $paysModel->select('nom');
+                $paysModel = new Pays;
+                $pays = $paysModel->select('nom');
 
-            $couleur = new Couleur;
-            $couleurs = $couleur->select('nom');
+                $couleur = new Couleur;
+                $couleurs = $couleur->select('nom');
 
-            if ($timbres) {
-                return View::render('enchere', ['encheres' => $encheres, 'images' => $images, 'timbres' => $timbres, 'conditions' => $conditions, 'pays' => $pays, 'couleurs' => $couleurs]);
-            } else {
-                return View::render('error');
+                $mise = new Mise;
+                $mises = $mise->select('montant');
+
+                if ($timbres) {
+                    return View::render('enchere', ['encheres' => $encheres, 'images' => $images, 'timbres' => $timbres, 'conditions' => $conditions, 'pays' => $pays, 'couleurs' => $couleurs, 'mises' => $mises]);
+                } else {
+                    return View::render('error');
+                }
             }
+            return View::render('error');          
         }
 }
